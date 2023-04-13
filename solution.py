743443c3546822312977b07ce08688bb24f27090
Vladimir <volodyabrus@gmail.com>
2023-04-13 22:36:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import anderson_ksamp
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+chat_id = 5437824033  # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return anderson_ksamp([x, y]).significance_level < 0.06
