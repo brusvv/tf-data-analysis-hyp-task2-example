500255f534509d19cf664c6d2f6fd715dab80bd0
Vladimir <volodyabrus@gmail.com>
2023-04-13 22:33:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5437824033 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return anderson_ksamp([x, y]).significance_level < 0.06
